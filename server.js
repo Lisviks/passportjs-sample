d7afb2ed82d9694accae48fa4f9b4f50cd7c030f
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2021-03-07 16:14:54 +0000

    MongoDB connection

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(
+  process.env.MONGO_DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  console.log('MongoDB connected')
+);
 
 const PORT = process.env.PORT || 5000;
 
